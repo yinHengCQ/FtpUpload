496f1e546982e41e538131cb50c48e620bf1c5ab
Administrator <Administrator@S63H9DFDCZP5YDZ>
2017-10-22 22:49:43 +0800

    download

--- src/ss.java
+++ src/ss.java
@@ -1,18 +1,23 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class ss {
 
+//	public static void main(String[] args) {
+//		FileInputStream in;
+//		try {
+//			in = new FileInputStream(new File("e:\\1.txt"));
+//			boolean flag = uploadFile.uploadFile("101.201.235.99", 21, "yinheng", "198708", "/root", "22.txt", in);
+//			System.out.println(flag);
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
 	public static void main(String[] args) {
-		FileInputStream in;
-		try {
-			in = new FileInputStream(new File("e:\\1.txt"));
-			boolean flag = uploadFile.uploadFile("101.201.235.99", 21, "yinheng", "198708", "/root", "22.txt", in);
-			System.out.println(flag);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		boolean flag = uploadFile.downFile("101.201.235.99", 21, "yinheng", "198708", "/home/yinheng", "22.txt", "e:\\");
+		System.out.println(flag);
 	}
 }
